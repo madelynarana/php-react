c2c4df6f98f95fa0370cfeb94c6aacfc727c88fb
Madelyn Arana <madelynarana@gmail.com>
2023-04-08 02:03:28 -0600

    Añadir conexión a base de datos.

---
+++ backend/Model.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'Config.php'; // Colocar información de la base de datos.
+
+if (DATABASE == '' || IP == '' || USERNAME == '' || PASSWORD == '') {
+    echo 'Check the <code>config.php</code>';
+}
+
+class DataBase{
+
+    private $database = DATABASE;
+    private $ip = IP;
+    private $username = USERNAME;
+    private $password = PASSWORD;
+
+    function Connection(){
+        try {
+
+            $conn = new PDO("mysql:host=$this->ip;dbname=$this->database", $this->username, $this->password);
+            echo "Connected to $this->database at $this->ip successfully.";
+            return $conn;
+
+        } catch (PDOException $pe) {
+
+            die("Could not connect to the database $this->database :" . $pe->getMessage());
+
+        }
+    }
+}
